145e02757d03c32a0669ac7da666e886652fc5f6
Ivan Letov <letov88@gmail.com>
2023-04-25 12:55:49 +0300

    Hero Section

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,14 +1,14 @@
 import { createClient } from "@sanity/client";
-import { ImageUrlBuilder } from "next-sanity-image";
+import createImageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: process.env.NEXT_PUBLICK_SANITY_ID,
+    projectId: '6oluk6fi',
     dataset: 'production',
     apiVersion: '2023-04-25',
     useCdn: true,
     token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
 
-const builder = ImageUrlBuilder(client);
+const builder = createImageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
