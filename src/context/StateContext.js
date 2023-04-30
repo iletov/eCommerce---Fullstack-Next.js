255cdda23cb4a2e907fe54403e37f15d699a0ac1
Ivan Letov <letov88@gmail.com>
2023-04-30 23:55:30 +0300

    Cart Component

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -10,6 +10,9 @@ export const StateContext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
+  let foundProduct;
+  let index;
+
 // ----------Add products to the cart---------
 
   const onAdd = (product, quantity) => {
@@ -37,6 +40,34 @@ export const StateContext = ({ children }) => {
     toast.success(`${qty} ${product.name} added to the cart.`)
   }
 
+  // ---------Add items from the cart-----
+
+  const togglCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+    
+    const newCartItems = cartItems.filter((item) => item._id !== id);
+
+    if(value === 'inc') {
+      let changeCartItems = [...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }]
+      setCartItems(changeCartItems)
+
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+
+      setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+
+    } else if(value === 'dec') {
+      if(foundProduct.quantity > 1) {
+        let changeCartItems = [...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }]
+        setCartItems(changeCartItems)
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+      }  
+    }
+  }
+
   // --------- change quantity ---------
 
   const incQty = () => {
@@ -56,7 +87,7 @@ export const StateContext = ({ children }) => {
     <Context.Provider
       value={{
         showCart, setShowCart, cartItems, totalPrice, totalQuantities, qty,
-        incQty, decQty, onAdd, 
+        incQty, decQty, onAdd, togglCartItemQuantity,
       }}
     >
       {children}
